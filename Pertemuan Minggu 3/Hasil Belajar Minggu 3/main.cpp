4e716b2063623a5637a228e98f9890ca21b77df8
M Ilham Ramadhani <Irmdhani>
2022-09-19 20:09:16 +0700

    Hasil Belajar Minggu 3

---
+++ Pertemuan Minggu 3/Hasil Belajar Minggu 3/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Title
+    cout << "Hello All!" << endl;
+    printf("Today is About Lesson at Weeks 3 \n");
+
+    //Variable
+    int x; // Deklarasi
+    x = 19; //Assigns
+    float y = 173.4; // Inisialisasi
+    char z = 'S';
+    string zz = "XXL";
+
+    //Programs
+    cout << "Umur Saya : " << x;
+    cout << "" << endl;
+    cout << "\nTinggi Badan Saya : " << y;
+    cout << "\nUkuran Baju Saya : " << z;
+    cout << "\nTeman Saya Ukuran Bajunya : " << zz;
+
+    int a;
+    cout << "\nX = 19 Masukkan Angka :" << endl;
+    cin >> a;
+    cout << "X + A =" << a + x;
+
+    /*
+    Mohamad Ilham Ramadhani
+    A11.2022.14587 | 4116
+    */
+
+    return 0;
+}
