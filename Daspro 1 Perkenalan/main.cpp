c47b72478d401c274186e55f586a472dc09da401
M Ilham Ramadhani <Irmdhani>
2022-09-19 19:30:20 +0700

    Upload By GIT

---
+++ Daspro 1 Perkenalan/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //First Sentence
+    cout << "Hello Everyone!" << endl;
+    cout << "How Are You Today?" << endl;
+    cout << "I'm Mohamad Ilham Ramadhani" << endl;
+    cout << "I Study At Udinus University With NIM A11.2022.14587" << endl;
+    cout << "I Was Born At November 19 2002" << endl;
+    cout << "I Live at Sampangan Street, Semarang City" << endl;
+    cout << "Well That's all from me" << endl;
+    cout << "Thanks For opening my first Program" << endl;
+
+    return 0;
+}
